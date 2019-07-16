1deadc298871356a6ce761109021089703fec170
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-16 17:31:47 -0400

    update shoppinglist with methods, still failing

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -75,13 +75,7 @@ const shoppingList = (function(){
       render();
     });
   }
-  
-  function toggleCheckedForListItem(id) {
-    const foundItem = store.items.find(item => item.id === id);
-    foundItem.checked = !foundItem.checked;
-  }
-  
-  
+
   function getItemIdFromElement(item) {
     return $(item)
       .closest('.js-item-element')
@@ -91,19 +85,9 @@ const shoppingList = (function(){
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
       const id = getItemIdFromElement(event.currentTarget);
-      toggleCheckedForListItem(id);
+      store.findAndToggleChecked(id);
       render();
     });
-  }
-  
-  function deleteListItem(id) {
-    const index = store.items.findIndex(item => item.id === id);
-    store.items.splice(index, 1);
-  }
-  
-  function editListItemName(id, itemName) {
-    const item = store.items.find(item => item.id === id);
-    item.name = itemName;
   }
   
   function toggleCheckedItemsFilter() {
@@ -114,14 +98,13 @@ const shoppingList = (function(){
     store.searchTerm = val;
   }
   
-  
   function handleDeleteItemClicked() {
     // like in `handleItemCheckClicked`, we use event delegation
     $('.js-shopping-list').on('click', '.js-item-delete', event => {
       // get the index of the item in store.items
       const id = getItemIdFromElement(event.currentTarget);
       // delete the item
-      deleteListItem(id);
+      store.findAndDelete(id);
       // render the updated shopping list
       render();
     });
@@ -132,7 +115,7 @@ const shoppingList = (function(){
       event.preventDefault();
       const id = getItemIdFromElement(event.currentTarget);
       const itemName = $(event.currentTarget).find('.shopping-item').val();
-      editListItemName(id, itemName);
+      store.findAndUpdateName(id, itemName);
       render();
     });
   }
