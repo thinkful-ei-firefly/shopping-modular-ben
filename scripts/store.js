e4c6600838bbc11b970b63866112fcf418db7e39
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-16 17:29:09 -0400

    update store with methods, though still failing

--- scripts/store.js
+++ scripts/store.js
@@ -1,6 +1,40 @@
 /* eslint-disable */
+
+function findById(id) {
+  store.items.find(item => item.id === id);
+}
+
+function addItem(name) {
+  try {
+    Item.validateName(name);
+    this.items.push(Item.create(itemName));
+  } catch (error) {
+    console.log(`Cannot add item: ${error.message}`);
+  }
+}
+
+function findAndToggleChecked(id) {
+  const checked = this.items.findById(id).checked;
+  checked = !checked;
+}
+
+function findAndUpdateName(id, newName) {
+  try {
+    Item.validateName(name);
+    const name = this.items.findById(id).name;
+    name = newName;
+  } catch (error) {
+    console.log(`Cannot update name: ${error.message}`);
+  }
+}
+
+function findAndDelete(id) {
+  const filterItem = this.items.findById(id);
+  this.items.filter(item => item !== filterItem);
+}
+
 const store = (function() {
-    const items = [
+  const items = [
     { id: cuid(), name: 'apples', checked: false },
     { id: cuid(), name: 'oranges', checked: false },
     { id: cuid(), name: 'milk', checked: true },
@@ -9,5 +43,14 @@ const store = (function() {
   const hideCheckedItems = false;
   const searchTerm = '';
 
-  return { items, hideCheckedItems, searchTerm };
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm,
+    findById: findById(),
+    addItem: addItem(),
+    findAndToggleChecked: findAndToggleChecked(),
+    findAndUpdateName: findAndUpdateName(),
+    findAndDelete: findAndDelete()
+  };
 })();
