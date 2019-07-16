b4599630fafd67fcb39724289ef21ae0cc92b5b8
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-16 17:09:20 -0400

    item factory and validator -- needs work still?

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,4 +1,17 @@
 /* eslint-disable */
+function validateName(name) {
+  if (name == undefined) {
+    throw new TypeError('Name does not exist.');
+  }
+}
+
+function create(name) {
+  return { id: cuid(), name: name, checked: false };
+}
+
 const Item = (function() {
-  return {};
+  return {
+    validateName: validateName(),
+    create: create()
+  };
 })();
